d58ec712232aaf212c854a31a4ac869b56523f18
nghiahsgs <nghiahsgs@gmail.com>
2020-05-19 10:07:26 +0700

    make it better

---
+++ game2.py
@@ -0,0 +1,42 @@
+from random import randint
+
+def gen_lucky_number():
+    return randint(0,99)>50
+
+
+
+my_money=1000
+unitmoney=1
+list_money=[]
+
+# INITAL VALUE
+isTai=gen_lucky_number()
+isPlayTai=False
+
+for i_phien in range(1,100):
+    if isTai:
+        #play up
+        isPlayTai=True
+    else:
+        #play down
+        isPlayTai=False
+    my_money=my_money-1
+    isTai=gen_lucky_number()
+    
+    if isTai:
+        if isPlayTai:
+            my_money = my_money + 2
+    if not isTai:
+        if not isPlayTai:
+            my_money = my_money + 2
+    list_money.append(my_money)
+    print('isPlayTai',isPlayTai)
+    print('isTai',isTai)
+    print('my_money',my_money)
+    print('-----------------------------')
+
+print('min_money:',min(list_money))
+print('max_money:',max(list_money))
+
+
+
